0737c9e9a0af78bec17602979b2b84e35a4f2f85
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2026-05-16 19:27:20 -0300

    funcionando tudo

--- src/pipeline/transform.py
+++ src/pipeline/transform.py
@@ -1,10 +1,30 @@
 import pandas as pd
-from typing import List
 
 
 """
 função para transformar os dataframes extraídos para um único dataframe
 """
 
-def contact_data_frames(data_frame_list: list[pd.DataFrame]) -> pd.DataFrame:
-    return pd.concat(data_frame_list, ignore_index=True)
+def contact_data_frames(
+    data_frame_list: list[pd.DataFrame]
+) -> pd.DataFrame:
+
+    # concatena os dataframes
+    df = pd.concat(
+        data_frame_list,
+        ignore_index=True
+    )
+
+    total_before = len(df)
+
+    # remove registros duplicados
+    df = df.drop_duplicates()
+
+    total_after = len(df)
+
+    print(
+        f"Duplicados removidos: "
+        f"{total_before - total_after}"
+    )
+
+    return df

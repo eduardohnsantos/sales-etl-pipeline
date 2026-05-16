b70fbb3c4d1766d79e3ccf9ba9b6014467b3b6df
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2026-05-16 17:14:47 -0300

    load funcionando

--- src/main.py
+++ src/main.py
@@ -1,4 +1,26 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import contact_data_frames
+from pipeline.load import load_excel
 
-lista_de_dataframes = extract_from_excel("data/raw")
-print(lista_de_dataframes)
+
+if __name__ == "__main__":
+
+    print("Iniciando pipeline ETL...\n")
+
+    data_frame_list = extract_from_excel("data/raw")
+
+    print(f"Arquivos extraídos: {len(data_frame_list)}")
+
+    data_frame = contact_data_frames(data_frame_list)
+
+    print(f"Total de registros consolidados: {len(data_frame)}")
+
+    resultado = load_excel(
+        data_frame,
+        "data/trusted",
+        "trusted"
+    )
+
+    print(resultado)
+
+    print("\nPipeline executado com sucesso!")
---
+++ src/pipeline/load.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+
+"""
+receber um dataframe e salvar em um arquivo excel
+
+args: 
+data_frame (pd.DataFrame): dataframe a ser salvo
+output_path (str): caminho onde o arquivo excel será salvo
+file_name (str): nome do arquivo excel a ser salvo
+
+return: "Arquivo salvo com sucesso!" ou "Erro ao salvar o arquivo: {error}"
+"""
+
+def load_excel(
+    data_frame: pd.DataFrame,
+    output_path: str,
+    file_name: str
+) -> str:
+
+    try:
+
+        # cria a pasta caso não exista
+        os.makedirs(output_path, exist_ok=True)
+
+        output_file = f"{output_path}/{file_name}.xlsx"
+
+        data_frame.to_excel(output_file, index=False)
+
+        return f"Arquivo salvo com sucesso em: {output_file}"
+
+    except Exception as e:
+
+        return f"Erro ao salvar o arquivo: {e}"
